543b76e5be3b5f8337925c45a9226b5dbc6a0a42
Talisson Melo <talis.melo@petize.com.br>
2025-01-18 09:54:29 -0300

    Componentizing buttons and inputText

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -6,6 +6,8 @@ import { useImageService } from "../services/image/image.service";
 import { useState } from "react";
 import { Image } from "../services/image/image.resource";
 import Link from "next/link";
+import { Button } from "@/components/button/Button";
+import { InputText } from "@/components/input/InputText";
 
 export default function GalleryPage() {
   const useService = useImageService();
@@ -38,11 +40,9 @@ export default function GalleryPage() {
     <Template loading={loading}>
       <section className="flex flex-col items-center justify-center my-5">
         <div className="flex space-x-4">
-          <input
-            onChange={(event) => setQuery(event.target.value)}
-            type="text"
-            className="border ox-3 py-2 rounded-lg text-gray-900"
+          <InputText
             placeholder="Type Name or Tags"
+            onChange={(event) => setQuery(event.target.value)}
           />
           <select
             onChange={(event) => setExtension(event.target.value)}
@@ -53,16 +53,15 @@ export default function GalleryPage() {
             <option value="JPEG">JPEG</option>
             <option value="GIF">GIF</option>
           </select>
-          <button
-            className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-300"
+
+          <Button
+            style="bg-blue-500 hover:bg-blue-300"
+            label="Search"
             onClick={searchImages}
-          >
-            Search
-          </button>
+          />
+
           <Link href="/form">
-            <button className="bg-green-500 text-gray-900 px-4 py-2 rounded-lg hover:bg-green-300">
-              Add New
-            </button>
+            <Button style="bg-yellow-500 hover:bg-yellow-300" label="Add New" />
           </Link>
         </div>
       </section>
