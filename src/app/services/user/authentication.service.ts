509f2547af100662b0e63b2178c1655d35f75367
Talisson Melo <talis.melo@petize.com.br>
2025-01-23 08:08:09 -0300

    Created User Service

---
+++ src/app/services/user/authentication.service.ts
@@ -0,0 +1,39 @@
+import { AccessToken, Credentials, User } from "./users.service";
+
+class AuthService {
+  baseURL: string = "http://localhost:8080/v1/users";
+  static AUTH_PARAM: string = "_authenticate";
+
+  async authenticate(credentials: Credentials): Promise<AccessToken> {
+    const response = await fetch(this.baseURL + "/authenticate", {
+      method: "POST",
+      body: JSON.stringify(credentials),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.status == 401) {
+      throw new Error("User or password are incorrect!");
+    }
+
+    return await response.json();
+  }
+
+  async save(user: User): Promise<void> {
+    const response = await fetch(this.baseURL, {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.status == 409) {
+      const responseError = await response.json();
+      throw new Error(responseError.error);
+    }
+  }
+}
+
+export const useAuth = () => new AuthService();
---
+++ src/app/services/user/users.service.ts
@@ -0,0 +1,14 @@
+export class User {
+  name?: string;
+  email?: string;
+  password?: string;
+}
+
+export class Credentials {
+  email?: string;
+  password?: string;
+}
+
+export class AccessToken {
+  accessToken?: string;
+}
