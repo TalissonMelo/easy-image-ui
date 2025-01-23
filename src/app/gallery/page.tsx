a779c7649c06a4dc82a7f239aabbabbd22e459e0
Talisson Melo <talis.melo@petize.com.br>
2025-01-23 08:35:58 -0300

    Valid router in page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { Button } from "@/components/button/Button";
 import { InputText } from "@/components/input/InputText";
 import { useNotification } from "@/components/notification";
+import { AuthenticatedPage } from "@/components/AuthenticatedPage";
 
 export default function GalleryPage() {
   const useService = useImageService();
@@ -43,37 +44,42 @@ export default function GalleryPage() {
   }
 
   return (
-    <Template loading={loading}>
-      <section className="flex flex-col items-center justify-center my-5">
-        <div className="flex space-x-4">
-          <InputText
-            placeholder="Type Name or Tags"
-            onChange={(event) => setQuery(event.target.value)}
-          />
-          <select
-            onChange={(event) => setExtension(event.target.value)}
-            className="border px-4 py-2 rounded-lg text-gray-900"
-          >
-            <option value="">All formats</option>
-            <option value="PNG">PNG</option>
-            <option value="JPEG">JPEG</option>
-            <option value="GIF">GIF</option>
-          </select>
+    <AuthenticatedPage>
+      <Template loading={loading}>
+        <section className="flex flex-col items-center justify-center my-5">
+          <div className="flex space-x-4">
+            <InputText
+              placeholder="Type Name or Tags"
+              onChange={(event) => setQuery(event.target.value)}
+            />
+            <select
+              onChange={(event) => setExtension(event.target.value)}
+              className="border px-4 py-2 rounded-lg text-gray-900"
+            >
+              <option value="">All formats</option>
+              <option value="PNG">PNG</option>
+              <option value="JPEG">JPEG</option>
+              <option value="GIF">GIF</option>
+            </select>
 
-          <Button
-            style="bg-blue-500 hover:bg-blue-300"
-            label="Search"
-            onClick={searchImages}
-          />
+            <Button
+              style="bg-blue-500 hover:bg-blue-300"
+              label="Search"
+              onClick={searchImages}
+            />
 
-          <Link href="/form">
-            <Button style="bg-yellow-500 hover:bg-yellow-300" label="Add New" />
-          </Link>
-        </div>
-      </section>
-      <section className="grid grid-cols-4 gap-8">
-        {images.map(renderImageCard)}
-      </section>
-    </Template>
+            <Link href="/form">
+              <Button
+                style="bg-yellow-500 hover:bg-yellow-300"
+                label="Add New"
+              />
+            </Link>
+          </div>
+        </section>
+        <section className="grid grid-cols-4 gap-8">
+          {images.map(renderImageCard)}
+        </section>
+      </Template>
+    </AuthenticatedPage>
   );
 }
