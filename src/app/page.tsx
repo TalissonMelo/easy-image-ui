e02113c5501c7bde027508ef48ab9ce22a8e17ea
Talisson Melo <talis.melo@petize.com.br>
2025-01-27 08:00:03 -0300

    Setting the application home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import GalleryPage from "./gallery/page";
+import Login from "./login/page";
+import { useAuth } from "./services/user/authentication.service";
+
 export default function Home() {
-  return <></>;
+  const auth = useAuth();
+  const user = auth.getUserSession();
+
+  if (!user) {
+    return <Login />;
+  }
+
+  return <GalleryPage />;
 }
